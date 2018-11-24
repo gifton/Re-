cfb3f8f637ad14933c32852a26f14d86a0feec03
gifton <giftono@gmail.com>
2018-11-24 02:42:46 -0800

    updated tiles with styling

--- Resaturate/HomeView.swift
+++ Resaturate/HomeView.swift
@@ -27,6 +27,16 @@ class HomeView: UIView {
     let logoutLabel = UILabel()
     let connectionsLabel = UILabel()
     
+    let linksLabel = UILabel()
+    
+    let noteOval = UIView()
+    let noteImage = UIImageView()
+    let noteLabel = UILabel()
+    
+    let photoBar = UIView()
+    let photoLabel = UILabel()
+    let newPhotoButton = UIButton()
+    
     private func buildLeftView() {
         addSubview(connectionView)
         addSubview(logoutView)
@@ -34,27 +44,34 @@ class HomeView: UIView {
         
         logoutView.addSubview(logoutLabel)
         connectionView.addSubview(connectionsLabel)
-        styleItems()
-        placeItems()
+        styleLeftItems()
+        placeLeftItems()
     }
     
-    func placeItems() {
+    func placeLeftItems() {
+        //views
         connectionView.frame = CGRect(x: 0, y: 0, width: ScreenSize.SCREEN_WIDTH * 0.21, height: ScreenSize.SCREEN_HEIGHT * 0.675)
-        logoutView.frame = CGRect(x: 0, y: (ScreenSize.SCREEN_HEIGHT * 0.675) + 33, width: ScreenSize.SCREEN_WIDTH * 0.21, height: ScreenSize.SCREEN_HEIGHT * 0.15)
+        logoutView.frame = CGRect(x: 0, y: (ScreenSize.SCREEN_HEIGHT * 0.675) + 33.5, width: ScreenSize.SCREEN_WIDTH * 0.21, height: ScreenSize.SCREEN_HEIGHT * 0.15)
         
+        //labels
         reLabel.translatesAutoresizingMaskIntoConstraints = false
-        reLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
-        reLabel.topAnchor.constraint(equalTo: logoutView.bottomAnchor, constant: 25).isActive = true
-        
         logoutLabel.translatesAutoresizingMaskIntoConstraints = false
-        logoutLabel.leadingAnchor.constraint(equalTo: logoutView.leadingAnchor, constant: 2).isActive = true
-        logoutLabel.topAnchor.constraint(equalTo: logoutView.topAnchor, constant: 2).isActive = true
-        print (logoutView.center)
+        connectionsLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        logoutLabel.setAnchor(top: logoutView.topAnchor, leading: logoutView.leadingAnchor, bottom: logoutView.bottomAnchor, trailing: logoutView.trailingAnchor, paddingTop: 0, paddingLeading: 0, paddingBottom: 0, paddingTrailing: 0)
+        
+        connectionsLabel.transform = CGAffineTransform(rotationAngle: -(CGFloat.pi / 2))
+        NSLayoutConstraint.activate ([
+            reLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            reLabel.topAnchor.constraint(equalTo: logoutView.bottomAnchor, constant: 25),
+            connectionsLabel.centerXAnchor.constraint(equalTo: connectionView.centerXAnchor, constant: 0),
+            connectionsLabel.centerYAnchor.constraint(equalTo: connectionView.centerYAnchor, constant: 100),
+        ])
     }
-    func styleItems() {
+    func styleLeftItems() {
         connectionView.backgroundColor = .white
         logoutView.backgroundColor = UIColor(hex: "F96969")
-        reLabel.text = "Re:"
+        reLabel.text = " Re:"
         reLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 22)
         reLabel.textColor = .white
         reLabel.layer.borderWidth = 2
@@ -64,8 +81,13 @@ class HomeView: UIView {
         
         logoutLabel.numberOfLines = 3
         logoutLabel.text = "LO\nGO\nUT"
-        logoutLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 35)
+        logoutLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 30)
         logoutLabel.textColor = .black
+        logoutLabel.textAlignment = .center
+        
+        connectionsLabel.text = "C O N N E C T I O N S"
+        connectionsLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 30)
+        connectionsLabel.textColor = .black
     }
     
     func addTiles() {
@@ -75,16 +97,107 @@ class HomeView: UIView {
             addSubview(tile)
             tile.frame = CGRect(x: ScreenSize.SCREEN_WIDTH * 0.315, y: yAxis, width: ScreenSize.SCREEN_WIDTH - (ScreenSize.SCREEN_WIDTH * 0.315), height: ScreenSize.SCREEN_HEIGHT * 0.315)
             
-            yAxis += (ScreenSize.SCREEN_HEIGHT * 0.315) + 33
+            yAxis += (ScreenSize.SCREEN_HEIGHT * 0.315) + 34
         }
-        addContextToTiles()
+        styleTiles()
+        setupLinks()
+        setupNotes()
+        setupPhotos()
     }
     
     
-    func addContextToTiles() {
+    fileprivate func styleTiles() {
         photosView.backgroundColor = UIColor(hex: "5066E3")
         linksView.backgroundColor = UIColor(hex: "35B498")
         notesView.backgroundColor = UIColor(hex: "FCA07B")
+        
+        linksLabel.text = "LINKS"
+        linksLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 30)
+        linksLabel.textColor = .white
+        
+        noteImage.image = .random()
+        noteOval.backgroundColor = .white
+        noteLabel.textAlignment = .center
+        noteLabel.text = "NOTES"
+        noteLabel.font = UIFont(name: "HelveticaNeue-Bold", size: fontSize.xXLarge.rawValue)
+        
+        photoBar.setHeightWidth(width: 235, height: 33)
+        photoBar.backgroundColor = .black
+        photoLabel.text = "Photos"
+        photoLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 70)
+        photoLabel.textColor = .white
+        newPhotoButton.setHeightWidth(width: 235, height: 33)
+        newPhotoButton.backgroundColor = .white
+        newPhotoButton.setTitle("New", for: .normal)
+        
+    }
+    fileprivate func setupNotes() {
+        let ovalRadii: CGFloat = 75
+        notesView.addSubview(noteOval)
+        
+        noteOval.makeCircle(size: ovalRadii * 2.0)
+        
+        noteOval.frame = CGRect(x: (notesView.frame.width / 2) - ovalRadii, y: (photosView.frame.height / 2 - ovalRadii - 25), width: ovalRadii * 2.0, height: ovalRadii * 2.0)
+        
+        noteOval.addSubview(noteImage)
+        noteOval.addSubview(noteLabel)
+        noteImage.frame = CGRect(x: 0, y: 0, width: ovalRadii * 2, height: ovalRadii)
+        noteImage.layer.masksToBounds = true
+        noteLabel.frame = CGRect(x: 0, y: 75, width: ovalRadii * 2.0, height: ovalRadii)
+    }
+    
+    fileprivate func setupLinks() {
+        let linkImg = UIImage(named: "airplane")
+        let linkViewSize = linksView.frame
+        var startingX: CGFloat = 0.0
+        var startingY: CGFloat = 150
+        let numberOfPlanes: CGFloat = linkViewSize.width / 25
+        
+        func addPlanes() {
+            var counter = 0
+            while counter < 2 {
+                for _ in stride(from: 0, to: numberOfPlanes + 1, by: 1) {
+                    let img: UIImageView = {
+                        let img = UIImageView()
+                        img.image = linkImg
+                        img.contentMode = .scaleToFill
+                        
+                        return img
+                    }()
+                    linksView.addSubview(img)
+                    img.frame = CGRect(x: startingX, y: startingY, width: 25, height: 20)
+                    startingX += 30
+                }
+                counter += 1
+                startingX = 0
+                startingY += 45
+            }
+        }
+        addPlanes()
+        linksView.addSubview(linksLabel)
+        linksLabel.translatesAutoresizingMaskIntoConstraints = false
+        linksLabel.centerYAnchor.constraint(equalTo: linksView.centerYAnchor, constant: 0).isActive = true
+        linksLabel.centerXAnchor.constraint(equalTo: linksView.centerXAnchor, constant: 0).isActive = true
+    }
+    
+    fileprivate func setupPhotos() {
+        let views = [photoBar, photoLabel, newPhotoButton]
+        var startingY: CGFloat = 50.0
+        for view in views {
+            photosView.addSubview(view)
+            view.translatesAutoresizingMaskIntoConstraints = false
+            if view === photoLabel {
+                view.centerXAnchor.constraint(equalTo: photoBar.centerXAnchor).isActive = true
+                view.topAnchor.constraint(equalTo: photosView.topAnchor, constant: startingY).isActive = true
+                continue
+            }
+            view.centerXAnchor.constraint(equalTo: photosView.centerXAnchor).isActive = true
+            view.topAnchor.constraint(equalTo: photosView.topAnchor, constant: startingY).isActive = true
+            
+            print(view.frame)
+            
+            startingY += 50
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
